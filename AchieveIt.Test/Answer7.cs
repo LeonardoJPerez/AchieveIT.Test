e5cdfef8a8fd9b5efec8d68e4a785e643e10bcf9
lol register <mecagoenti@mailinator.com>
2016-05-25 18:06:44 -0400

    Added answer for question #13
    Added NUnit test for answer #13.

---
+++ AchieveIt.Test/Answer7.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Dapper;
+
+namespace AchieveIt.Test
+{
+    [TestFixture]
+    public class Answer7
+    {
+        [Test]
+        public void PrintEmployees()
+        {
+            using (var conn = new SqlConnection("Server=sqlinstance.cr39h6nxoxnt.us-east-1.rds.amazonaws.com;Database=AchieveIT;User Id=sa;Password=diamondsword.987;"))
+            {
+                conn.Open();
+
+                var employees = conn.Query<Employee>("SELECT e.name as 'EmployeeName', mngr.name as 'ManagerName' FROM employee e LEFT JOIN employee mngr on e.mgr_id = mngr.id;").ToList();
+                var sb = new StringBuilder();
+
+                sb.AppendLine("<table>");
+                sb.AppendLine("\t<thead>\n\t\t<tr>");
+                sb.AppendLine("\t\t\t<th>Employee Name</th>");
+                sb.AppendLine("\t\t\t<th>Manager Name</th>");
+                sb.AppendLine("\t\t</tr>\n\t</thead>");
+
+                sb.AppendLine("\t<tbody>");
+                foreach (var emp in employees)
+                {                    
+                    sb.AppendLine("\t\t<tr>");
+                    sb.AppendLine($"\t\t\t<td>{emp.EmployeeName}</td>");
+                    sb.AppendLine($"\t\t\t<td>{emp.ManagerName}</td>");
+                    sb.AppendLine("\t\t</tr>");                     
+                }
+                sb.AppendLine("\t</tbody>");
+                sb.AppendLine("</table>");
+                conn.Close();
+
+                Console.Write(sb.ToString());
+            }
+        }
+    }
+
+
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+
+        public string ManagerName { get; set; }
+    }
+}
