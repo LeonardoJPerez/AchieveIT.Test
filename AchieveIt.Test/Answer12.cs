c3a6c6880a4fe21a2d91e55443c8559654e416fa
Leonardo Perez <find.leonardo@gmail.com>
2016-05-25 22:57:52 -0400

    Fixed exit step in BinarySearch generic method (bonus method).

--- AchieveIt.Test/Answer12.cs
+++ AchieveIt.Test/Answer12.cs
@@ -42,7 +42,6 @@ namespace AchieveIt.Test
             return -1;
         }
 
-
         /// <summary>
         /// Binaries Search for an array of integers. Returns index of value or -1 if not found. Added as a bonus.
         /// </summary>
@@ -62,13 +61,22 @@ namespace AchieveIt.Test
 
             var comparer = Comparer<T>.Default;
             var res = comparer.Compare(midValue, searchValue);
+
+            if (max == min && res != 0)
+            {
+                // Item not found.
+                return -1;
+            }
+
             switch (res)
             {
                 case -1:
+                    // x is less than y.
                     // Search Right side.
                     return BinarySearch(sortedArray, searchValue, midPosition + 1, max);
 
                 case 1:
+                    // x is greater than y.
                     // Search Left side.
                     return BinarySearch(sortedArray, searchValue, min, midPosition - 1);
 
@@ -77,14 +85,14 @@ namespace AchieveIt.Test
             }
         }
 
-        private static List<List<int>> _sourceListsDouble = new List<List<int>> {
-            //new List<double>{ 7.9, 687.5, 345.6, 5318.6, 80330138, 813, 83, 834, 4, 3543.00, 3005.1, 51, 3585.9, 4, 3438.0, 4684, 438438, 4 },
-            //new List<double>{ 78, 569, 85, 665, 47, 527, 752, 4485, 320, 2, 545, 4, 52, 23, 63, 56, 5, 36363, 62, 3535   },
+        private static List<List<int>> _sourceListsInt = new List<List<int>> {
             new List<int>{ 100, 25, 84, 96, 55, 789, 66, 5, 2, 4, 7, 855, 24, 56, 96 }
         };
 
+        #region Integer Tests
+
         [Test]
-        [TestCaseSource("_sourceListsDouble")]
+        [TestCaseSource("_sourceListsInt")]
         public void TestBinarySearchInteger(List<int> list)
         {
             Console.Write("Unsorted: ");
@@ -106,15 +114,66 @@ namespace AchieveIt.Test
                 Console.Write($"{elem} ");
             }
 
-            Console.WriteLine($"Time: {timer.Elapsed}");
+            Console.WriteLine($"\nTime: {timer.Elapsed}");
+            Console.WriteLine();
 
             timer.Reset();
             timer.Start();
-            Console.WriteLine($"Searching for: {855}");
-            var valueIndex = BinarySearch(sortedList, 855);
+            Console.WriteLine($"Searching for: {859}");
+
+            var valueIndex = BinarySearch(sortedList, 859);
             timer.Stop();
+
             Console.WriteLine($"Value found: {valueIndex != -1} [index: {valueIndex}]");
             Console.WriteLine($"Time searching: {timer.Elapsed}");
         }
+
+        #endregion Integer Tests
+
+        #region Double Tests
+
+        private static List<List<double>> _sourceListsDouble = new List<List<double>> {
+            new List<double>{ 7.9, 687.5, 345.6, 5318.6, 80330138, 813, 83, 834, 4, 3543.00, 3005.1, 51, 3585.9, 4, 3438.0, 4684, 438438, 4 },
+            new List<double>{ 78, 569, 85, 665, 47, 527, 752, 4485, 320, 2, 545, 4, 52, 23, 63, 56, 5, 36363, 62, 3535   }
+        };
+
+        [Test]
+        [TestCaseSource("_sourceListsDouble")]
+        public void TestBinarySearchDouble(List<double> list)
+        {
+            Console.Write("Unsorted: ");
+            foreach (var elem in list)
+            {
+                Console.Write($"{elem} ");
+            }
+
+            var timer = new Stopwatch();
+            var sortAlgorithm = new Answer11();
+
+            timer.Start();
+            var sortedList = sortAlgorithm.QuickSort(list.ToArray());
+            timer.Stop();
+
+            Console.Write("\nSorted:   ");
+            foreach (var elem in sortedList)
+            {
+                Console.Write($"{elem} ");
+            }
+
+            Console.WriteLine($"\nTime: {timer.Elapsed}");
+            Console.WriteLine();
+
+            timer.Reset();
+            timer.Start();
+            Console.WriteLine($"Searching for: {23}");
+
+            var valueIndex = BinarySearch(sortedList, 23);
+            timer.Stop();
+
+            Console.WriteLine($"Value found: {valueIndex != -1} [index: {valueIndex}]");
+            Console.WriteLine($"Time searching: {timer.Elapsed}");
+        }
+
+        #endregion Double Tests
     }
 }
