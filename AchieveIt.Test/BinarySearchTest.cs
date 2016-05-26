c88a4cf4b6dcdb2cd0e03de9180e5df352811eec
Leonardo Perez <find.leonardo@gmail.com>
2016-05-25 21:41:43 -0400

    Fixed BinarySearch implementation. Added generic method.
    Updated answer #12 in text file.

--- AchieveIt.Test/BinarySearchTest.cs
+++ AchieveIt.Test/BinarySearchTest.cs
@@ -2,34 +2,69 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 
 namespace AchieveIt.Test
 {
     public class BinarySearchTest
     {
-        public int BinarySearch(int[] sortedArray, int searchValue)
+        /// <summary>
+        /// Binaries Search for an array of integers. Returns index of value or -1 if not found.
+        /// </summary>
+        /// <param name="sortedArray">The sorted array.</param>
+        /// <param name="searchValue">The search value.</param>
+        /// <param name="minPosition">The minimum position of the array.</param>
+        /// <param name="maxPosition">The maximum position.</param>
+        /// <returns></returns>
+        public static int BinarySearchInt(int[] sortedArray, int searchValue, int minPosition = 0, int maxPosition = -1)
         {
-            // Get middle value
-            var midPos = sortedArray.Length / 2;
-            var mid = sortedArray[midPos];
+            // Get middle value and boundaries.
+            var max = maxPosition == -1 ? sortedArray.Length - 1 : maxPosition;
+            var min = minPosition;
+            var midPosition = (max + min) / 2;
+            var midValue = sortedArray[midPosition];
 
-            if (mid < searchValue)
+            if (midValue < searchValue)
             {
                 // Search Right side.
-                return BinarySearch(sortedArray.Where((elem, i) => i > midPos).ToArray(), searchValue);
+                return BinarySearchInt(sortedArray, searchValue, midPosition + 1, max);
             }
-            else if (mid > searchValue)
+            else if (midValue > searchValue)
             {
                 // Search Left side.
-                return BinarySearch(sortedArray.Where((elem, i) => i < midPos).ToArray(), searchValue);
+                return BinarySearchInt(sortedArray, searchValue, min, midPosition - 1);
             }
-            else
+            else if (midValue == searchValue)
             {
-                return mid;
+                return midPosition;
             }
+
+            return -1;
         }
 
+        public int BinarySearch<T>(T[] sortedArray, T searchValue, int minPosition = 0, int maxPosition = -1)
+        {
+            // Get middle value
+            var max = maxPosition == -1 ? sortedArray.Length - 1 : maxPosition;
+            var min = minPosition;
+            var midPosition = (max + min) / 2;
+            var midValue = sortedArray[midPosition];
+
+            var comparer = Comparer<T>.Default;
+            var res = comparer.Compare(midValue, searchValue);
+            switch (res)
+            {
+                case -1:
+                    // Search Right side.
+                    return BinarySearch(sortedArray, searchValue, midPosition + 1, max);
+
+                case 1:
+                    // Search Left side.
+                    return BinarySearch(sortedArray, searchValue, min, midPosition - 1);
+
+                default:
+                    return midPosition;
+            }
+        }
 
         private static List<List<int>> _sourceListsDouble = new List<List<int>> {
             //new List<double>{ 7.9, 687.5, 345.6, 5318.6, 80330138, 813, 83, 834, 4, 3543.00, 3005.1, 51, 3585.9, 4, 3438.0, 4684, 438438, 4 },
@@ -64,10 +99,10 @@ namespace AchieveIt.Test
 
             timer.Reset();
             timer.Start();
-            Console.WriteLine($"Searching for: {789}");
-            var valueFound = BinarySearch(sortedList, 789);
+            Console.WriteLine($"Searching for: {855}");
+            var valueIndex = BinarySearch(sortedList, 855);
             timer.Stop();
-            Console.WriteLine($"Value found: {valueFound == 789} [{valueFound}]");
+            Console.WriteLine($"Value found: {valueIndex != -1} [index: {valueIndex}]");
             Console.WriteLine($"Time searching: {timer.Elapsed}");
         }
     }
